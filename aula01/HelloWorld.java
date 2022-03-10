e843a827f5f1a2073bcf64ab6c33142188a09f07
Matheus Romano Braga <romanobraga.m@gmail.com>
2022-03-09 22:01:21 -0300

    feat: update de code

--- aula01/HelloWorld.java
+++ aula01/HelloWorld.java
@@ -1,6 +1,11 @@
 package aula01;
 class HelloWorld {
     public static void main(String[] args) { 
-        System.out.println("Hello World!");
+        Greet("Matheus", "Romano");
+        
+    }
+
+    public static void Greet(String firstName, String lastName) {
+        System.out.printf("Hello:" + firstName + lastName);
     }
 }
